dc2989fdacf27bbd3a88719ff6a8094b71d5a74e
AbdusSamadAli <samad140804@gmail.com>
2024-11-19 14:21:33 +0530

    changes done

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-require("dotenv").config({ path: "../.env" });
+require("dotenv").config();
 const app = express();
 const server = http.createServer(app);
 const defaultValue = "<p>Initial content goes here...</p>";
