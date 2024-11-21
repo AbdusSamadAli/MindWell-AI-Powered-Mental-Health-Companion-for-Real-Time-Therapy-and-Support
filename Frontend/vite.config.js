ed357ee04c2fd427771a8c81cb78704274ecbc61
AbdusSamadAli <samad140804@gmail.com>
2024-11-21 22:17:47 +0530

    changes updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Define Vite config
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../Backend/dist',
+    // Adjust the output directory if needed
+    outDir: '../Backend/dist', // Ensures built files go to Backend/dist
+    emptyOutDir: true, // Ensures the output directory is cleaned before each build
   },
   server: {
-    port: 5173,
+    port: 5173, // Keeps the Vite dev server running on port 517
   },
 });
+
