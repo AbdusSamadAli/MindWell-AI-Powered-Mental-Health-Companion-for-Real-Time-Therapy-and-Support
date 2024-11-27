de517f885fb634bf0cd828dd2a47b3734252dfda
AbdusSamadAli <samad140804@gmail.com>
2024-11-27 21:25:11 +0530

    changes made

--- Frontend/src/signup.jsx
+++ Frontend/src/signup.jsx
@@ -22,7 +22,7 @@ const Signup = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:8080/api/signup",
+        "http://52.66.240.17:8080/api/signup",
         formData
       );
       setMessage(response.data.message); 
